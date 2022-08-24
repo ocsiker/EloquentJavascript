c77b4a227a95837dbd76ac5d44d6a2a507a01ed9
ocsiker <qthang89vn@gmail.com>
2022-08-24 11:05:08 +0700

    add function unless(test,then)

--- draftV1.js
+++ draftV1.js
@@ -10,6 +10,7 @@ repeat(5, i => {
   labels.push(`value of ${i + 1}`);
 });
 
+
 console.log(labels);
 
 function greaterThan(n) {
@@ -30,3 +31,14 @@ function noisy(f) {
 }
 
 noisy(Math.min)(1, 4, 3, 4, 56);
+
+function unless(test, then) {
+  if (!test) then();
+}
+//
+// unless(0, () => {
+// console.log('then function');
+//})
+repeat(6, i => {unless(i % 2 == 1, () => {
+            console.log(i, ' is even');
+          })});
