d031084a9b848adef22892a694a8a566cef62d64
ocsiker <qthang89vn@gmail.com>
2022-08-25 03:39:04 +0700

    how to know object prototype

--- objectPrototypes.js
+++ objectPrototypes.js
@@ -21,3 +21,10 @@ const harry = new Person('Harry');
 
 
 harry.get();
+
+// EXPLAIN
+// It's common to see this pattern, in which methods are defined on the
+// prototype, but data properties are defined in the constructor. That's because
+// methods are usually the same for every object we create, while we often want
+// each object to have its own value for its data properties (just as here where
+// every person has a different name).
